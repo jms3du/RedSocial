513cae01fddc019e8baa2051a392321af94ccb4d
jms3du <josemanuel.sevillano.edu@gmail.com>
2022-03-21 12:02:09 +0100

    Eliminación de mensajes automáticos en constructores autogenerados de excepciones.

--- src/com/jacaranda/memoryStorage/MemoryStorageException.java
+++ src/com/jacaranda/memoryStorage/MemoryStorageException.java
@@ -3,28 +3,24 @@ package com.jacaranda.memoryStorage;
 public class MemoryStorageException extends RuntimeException {
 
 	public MemoryStorageException() {
-		// TODO Auto-generated constructor stub
+		super();
 	}
 
 	public MemoryStorageException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
 	public MemoryStorageException(Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public MemoryStorageException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public MemoryStorageException(String message, Throwable cause, boolean enableSuppression,
 			boolean writableStackTrace) {
 		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
 	}
 
 }
--- src/com/jacaranda/publicacion/PublicacionException.java
+++ src/com/jacaranda/publicacion/PublicacionException.java
@@ -3,28 +3,24 @@ package com.jacaranda.publicacion;
 public class PublicacionException extends RuntimeException {
 
 	public PublicacionException() {
-		// TODO Auto-generated constructor stub
+		super();
 	}
 
 	public PublicacionException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
 	public PublicacionException(Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public PublicacionException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public PublicacionException(String message, Throwable cause, boolean enableSuppression,
 			boolean writableStackTrace) {
 		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
 	}
 
 }
